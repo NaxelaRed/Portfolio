7a6ecaeca30443cf7ca257fc3aa512f0f799432f
NaxelaRed <49093238+NaxelaRed@users.noreply.github.com>
2023-05-24 08:59:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.addEventListener('resize', function () {
 
 function clickMe(image) {
     var video = document.createElement('video');
-    video.src = 'https://rr3---sn-ab5sznly.googlevideo.com/videoplayback?expire=1684881067&ei=S-psZKXSEIW68wT7hIK4Cw&ip=181.177.89.166&id=o-AJZo-fegjmwHZgnCR6Br3uliPHM0dcEIK4S3BEXbU0W6&itag=22&source=youtube&requiressl=yes&mh=7c&mm=31%2C26&mn=sn-ab5sznly%2Csn-p5qs7nsr&ms=au%2Conr&mv=u&mvi=3&pl=23&pcm2=no&spc=qEK7B1VZKUx10cZAg6WtERquyvRZwWk&vprv=1&svpuc=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=212.091&lmt=1674233742818000&mt=1684858339&fvip=5&fexp=24007246%2C51000024&c=ANDROID&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cpcm2%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRAIgZCLoF2GIL0802hLA3qK-egQ3jJA0Vta4WoELOfZdYQwCIBqhIe4YEPc0pu8g_X9ViNujQBb7ZcwS50AAsflWI_Xk&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl&lsig=AG3C_xAwRQIhAIs8-S2CJSLBGpHaY9-9chxEDv6sI9BvaP4qh3y0ooYjAiBG4iP0O6b152Yit4W7D8aRE7QnbxtETuMrMt-8VBwyXA%3D%3D&title=Rick+Astley+-+Never+Gonna+Give+You+Up+%28Official+Music+Video%29';
+    video.src = 'https://www.cjoint.com/doc/22_03/LCxr0IDDV8o_rickroll.mp4';
     video.autoplay = true;
     video.loop = true;
     video.classList.add('you-got-rick-rolled');
